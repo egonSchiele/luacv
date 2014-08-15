0c95b32f1d6d9ab73ec043ec74c03f6df486585c
root <root@img01.local>
2014-08-15 19:03:40 +0000

    this is how you resize in lua+opencv

--- src/test.lua
+++ src/test.lua
@@ -1,6 +1,10 @@
 cv=require('luacv')
 
-src=cv.LoadImage("test.png", 0)
+src=cv.LoadImage("test.png")
 if (not src) then error("Can't load source image "..arg[1]) end
 
 print(cv.GetSize(src))
+
+mat = cv.CreateMat(200, 200, cv.CV_8UC3)
+cv.Resize(src, mat)
+cv.SaveImage("out.png", mat)
